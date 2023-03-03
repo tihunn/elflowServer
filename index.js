e48a25c8922996f2d8364ad77d12124ef9bb5cf2
tihunn <74502721+tihunn@users.noreply.github.com>
2023-03-03 12:36:46 +0300

    add dockignore, dock.prod. Try fix fileupload

--- index.js
+++ index.js
@@ -14,8 +14,8 @@ const host = process.env.SERVER_HOST
 const app = express()
 app.use(cors())
 app.use(express.json())
-app.use(express.static (path.resolve(__dirname, "static") ))
 app.use(fileUpload({}))
+app.use(express.static (path.resolve(__dirname, "static") ))
 app.use('/', mainRouter)
 
 app.use(errorHandler)
@@ -26,7 +26,7 @@ const start = async () => {
         await sequelize.authenticate()
         await sequelize.sync()
         console.log('Connection has been established successfully.');
-        console.log("env === SERVER_PORT" + process.env.SERVER_PORT)
+        console.log("env === SERVER_PORT: " + process.env.SERVER_PORT)
         app.listen( port, () => console.log(`Server listens http://${host}:${port}`) )
     } catch (e) {
         console.error('Unable to connect to the database:', e);
